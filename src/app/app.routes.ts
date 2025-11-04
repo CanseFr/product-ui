f1a417de463a6ffc32abe14c2e34a39e62920edc
canse <jucansell@gmail.com>
2025-11-04 10:36:23 +0100

    :sparkles: Create pathmatch redirect to

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,5 @@ import {AddProduct} from './pages/add-product/add-product';
 export const routes: Routes = [
   {path: 'products', component: Product},
   {path: 'add-product', component: AddProduct},
+  {path: '',redirectTo:"products",pathMatch:"full", },
 ];
