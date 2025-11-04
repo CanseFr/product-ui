e1bc5530d8cca1d875c89a8947d2c4907539ba97
canse <jucansell@gmail.com>
2025-11-04 10:34:54 +0100

    :sparkles: Create add-product page + path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {Product} from './pages/product/product';
+import {AddProduct} from './pages/add-product/add-product';
 
 export const routes: Routes = [
   {path: 'products', component: Product},
+  {path: 'add-product', component: AddProduct},
 ];
