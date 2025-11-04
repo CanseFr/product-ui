ea15995194fdeb2bf1e0c940c5f7e77d8b77e964
canse <jucansell@gmail.com>
2025-11-04 13:33:42 +0100

    :sparkles: Create add product form

--- src/app/pages/add-product/add-product.ts
+++ src/app/pages/add-product/add-product.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductType} from '../../models/product';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './add-product.html',
   styleUrl: './add-product.css',
 })
-export class AddProduct {
+export class AddProduct implements OnInit {
+
+  newProduct = new ProductType();
+
+  constructor() {
+  }
+
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+
+    addProduct(): void {
+    console.log(this.newProduct);
+    }
 
 }
