75290159abde892314e1205cd24dafd3ded1a7b1
canse <jucansell@gmail.com>
2025-11-07 09:27:45 +0100

    :sparkles: Create GUARD for admin routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,17 @@ import {SearchByCategory} from './pages/search-by-category/search-by-category';
 import {SearchByName} from './pages/search-by-name/search-by-name';
 import {Category} from './pages/category/category';
 import {Login} from './pages/login/login';
+import {NotFound} from './pages/not-found/not-found';
+import {productGuard} from './guards/product-guard';
 
 export const routes: Routes = [
   {path: 'products', component: Product},
-  {path: 'add-product', component: AddProduct},
-  {path: 'update-product/:id', component: UpdateProduct},
+  {path: 'add-product', component: AddProduct, canActivate:[productGuard] },
+  {path: 'update-product/:id', component: UpdateProduct, canActivate:[productGuard] },
   {path: 'search-by-cat', component: SearchByCategory},
   {path: 'search-by-name', component: SearchByName},
-  {path: 'list-cat', component: Category},
+  {path: 'list-cat', component: Category, canActivate:[productGuard]},
   {path: 'login', component: Login},
+  {path: 'not-found', component: NotFound},
   {path: '',redirectTo:"products",pathMatch:"full", },
 ];
---
+++ src/app/guards/product-guard.ts
@@ -0,0 +1,13 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {AuthenticationService} from '../services/authentication.service';
+import {inject} from '@angular/core';
+
+export const productGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthenticationService)
+  const router = inject(Router)
+  if (authService.isAdmin) return true
+  else {
+    router.navigate(["/not-found"])
+    return false;
+  }
+};
